ce2c5be3cfb13d42489993baf048794e3f244875
Giordano Francesco <giordan7@corp.generali.net>
2018-10-23 19:18:56 +0200

    Corretto un errore che non consentiva di restituire una response

--- handlers/create.js
+++ handlers/create.js
@@ -3,7 +3,7 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const v1 = require('uuid');
 
 //TODO: Manca la validazione degli oggetti inseriti nel DB
-exports.create = async (event) => {
+exports.create = async (event, context, callback) => {
     const now = Date.now();
     const data = JSON.parse(event.body);
     const params = {
@@ -15,5 +15,21 @@ exports.create = async (event) => {
             updatedAt: now
         }
     };
-    return await dynamoDb.put(params).promise();
+    try {
+        const result = await dynamoDb.put(params).promise();
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result),
+        };
+        callback(null, response);
+    } catch (err) {
+        console.error(err);
+        callback(null, {
+            statusCode: err.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Couldn\'t delete the patient item.',
+        });
+    }
+
+
 }
